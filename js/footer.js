a0cedec28e41ba1a2bb8dd468797894581cfd7aa
josue <you@example.com>
2019-07-30 14:43:18 -0400

    home footer

---
+++ js/footer.js
@@ -0,0 +1,39 @@
+Vue.component('footer-1',{
+    template:  //html 
+    `
+<div>    
+<div class="container">
+<div class="content">
+    <div class="item item-0"><h5>Inicio</h5></div>
+    <div class="item item-1"><h5>Sobre Nosotros</h5></div>
+    <div class="item item-2"><h5>Preguntas Frecuentes</h5></div>
+    <div class="item item-3"><h5>Contactos</h5></div>
+    <div class="item item-4"><h5>Enlaces de Interes</h5>
+         <a href="">presidencia de la republica</a>
+         <a href="">vicepresidencia de la republica</a>
+         <a href="">ministerio de la presidencia</a>
+         <a href="">minisiterios de administracion publica</a>
+         <button class="btn">Ver Más</button>
+ </div>
+    <div class="item item-5">
+        <img src="assets/img/311.png" alt="">
+        <div class="social">
+            <img src="assets/img/facebook.png" alt="">
+            <img src="assets/img/twitter.png" alt="">
+            <img src="assets/img/insta.png" alt="">
+        </div>
+    </div>
+</div>
+<div class="divider"></div>
+<div class="sub-footer text-center">
+    <img src="assets/img/escudo-footer.png" alt="">
+    <h4>Consejo Nacional De competitividad</h4>
+     <p>Ave. Jiménez Moya #32, Edificio Franco Acra, 4to. Piso, Santo Domingo, R.D. </p>
+    <p>Tel.: <a href="#">(809) 476.7262</a> | mail: <a href="#">ejemplo@correo.com.do</a> | Fax:<a href="#"> (809) 563.0014</a></p> 
+    <p><a href="">Terminos de uso</a> | <a href="">Politicas</a> | <a href="">Preguntas Frecuentes</a></p>
+</div>
+</div>
+</div>
+`
+});
+
