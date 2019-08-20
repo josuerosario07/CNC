601587eda7fa8753f7129a67b67b998833967713
Josue Rosario <josuerosario@Josues-Mac-mini.local>
2019-08-20 11:38:30 -0400

    responsive-noticias

--- js/jquery-file.js
+++ js/jquery-file.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $("#sliderPrincipal").on("slide.bs.carousel", function(e) {
+$(document).ready(function () {
+  $("#sliderPrincipal").on("slide.bs.carousel", function (e) {
     var currentDataSlide = $('.slider-indicator[data-slide-to="' + e.to + '"]');
 
     var lastDataSlide = $('.slider-indicator[data-slide-to="' + e.from + '"]');
@@ -37,32 +37,35 @@ let currentCardId = null;
 let lastCardId = null;
 let isFirstClick = true;
 let divJdn = document.getElementById("bigSee");
-let btn = divJdn.getElementsByClassName("card-info");
-for (var i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", function() {
-    var cambio = document.getElementsByClassName("active-txt");
-    if (cambio.length > 0) {
-      cambio[0].className = cambio[0].className.replace("active-txt", " ");
-    }
+if (divJdn) {
+  let btn = divJdn.getElementsByClassName("card-info");
 
-    if (this.id != currentCardId || lastCardId != null) {
-      this.className += " active-txt";
-      isFirstClick = false;
+  for (var i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function () {
+      var cambio = document.getElementsByClassName("active-txt");
+      if (cambio.length > 0) {
+        cambio[0].className = cambio[0].className.replace("active-txt", " ");
+      }
+
+      if (this.id != currentCardId || lastCardId != null) {
+        this.className += " active-txt";
+        isFirstClick = false;
+        currentCardId = null;
+      }
+    });
+  }
+
+  function desactivarActiveClass(id) {
+    if (!isFirstClick) {
+      currentCardId = id;
+      let card = document.getElementById(`${id}`);
+      card.classList.remove("active-txt");
+      isFirstClick = true;
+      lastCardId = lastCardId != id ? (lastCardId = id) : null;
+    } else {
       currentCardId = null;
+      lastCardId = id;
     }
-  });
-}
-
-function desactivarActiveClass(id) {
-  if (!isFirstClick) {
-    currentCardId = id;
-    let card = document.getElementById(`${id}`);
-    card.classList.remove("active-txt");
-    isFirstClick = true;
-    lastCardId = lastCardId != id ? (lastCardId = id) : null;
-  } else {
-    currentCardId = null;
-    lastCardId = id;
   }
 }
 
